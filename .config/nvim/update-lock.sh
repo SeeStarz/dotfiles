082e0506a9343e1a366cdf74d19507758dbe58e0
SeeStarz <seestarz@tutamail.com>
2025-06-26 20:44:48 +0700

    Manual nvim plugins :)

---
+++ .config/nvim/update-lock.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+base=${XDG_DATA_HOME:-${HOME}/.local/share}/nvim/packages
+
+{
+for package in $(ls "${base}"); do
+  for kind in start opt; do
+  if [ -d "${base}/${package}/${kind}" ]; then
+    for repo in $(ls "${base}/${package}/${kind}"); do
+      cd "${base}/${package}/${kind}/${repo}"
+      url="$(git remote get-url origin)"
+      commit="$(git rev-parse HEAD)"
+      path="${package}/${kind}/${repo}"
+      jq -n --arg url "$url" --arg commit "$commit" --arg path "$path" \
+        '{url:$url, commit:$commit, path:$path}' | cat
+    done
+  fi
+  done
+done
+} | jq -s > package-lock.json
