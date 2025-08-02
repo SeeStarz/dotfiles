f8902a227e26b03ef389fe87b13a5700d6cdf551
SeeStarz <seestarz@tutamail.com>
2025-08-02 13:54:47 +0700

    Neovim delete COQ and CHADtree, replace with nvim-cmp

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,16 +1,5 @@
 -- Disable tab and shift tab bindings
 -- Disable no snippets warning
-vim.g.coq_settings = {
-  clients = {
-    snippets = {
-      warn = {},
-    },
-  },
-  keymap = {
-    recommended = false,
-  },
-}
-
 require("pywal16").setup()
 vim.cmd("colorscheme darkblue") -- I have no clue why this workaround is needed
 vim.cmd("colorscheme pywal16")
@@ -21,14 +10,40 @@ require("lualine").setup({
     },
 })
 
-local coq = require("coq")
-require("lspconfig")
-vim.cmd("COQnow --shut-up")
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+        vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+  },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+    }, {
+      { name = 'buffer' },
+    })
+  })
 
-vim.lsp.inlay_hint.enable(true)
-local config = coq.lsp_ensure_capabilities()
-config.capabilities.textDocument.completion.completionItem.resolveSupport.properties = {"additionalTextEdits", "command"}
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local config = {
+  capabilities = capabilities
+}
+
+require("lspconfig")
 vim.lsp.config("*", config)
+vim.lsp.inlay_hint.enable(true)
 vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("pylsp")
