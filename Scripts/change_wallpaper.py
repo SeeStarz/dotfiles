e62d6fe2e1288ecc7a07b3311d12cad8a65d902a
SeeStarz <seestarz@tutamail.com>
2025-08-03 12:28:26 +0700

    Wallpaper script refactor

--- Scripts/change_wallpaper.py
+++ Scripts/change_wallpaper.py
@@ -2,69 +2,97 @@
 
 import argparse
 from os import getenv, makedirs, symlink, remove
-from os.path import expandvars, isfile
+from os.path import expanduser, isfile
 from subprocess import run
 from sys import stderr
 
-# Argument parser setup
-parser = argparse.ArgumentParser(
-    description="Apply a pywal-compatible theme from a JSON colorscheme."
-)
-parser.add_argument(
-    "wallpaper",
-    help="wallpaper filename (with extension) used to identify theme name."
-)
-parser.add_argument(
-    "-l", "--light",
-    action="store_true",
-    help="Use light variant of the colorscheme (default: dark variant)"
-)
-parser.add_argument(
-    "--dry-run",
-    action="store_true",
-    help="Print the resolved path and command without applying theme"
-)
+class Dirs:
+    wallpaper: str = ""
+    hyprpaper_state: str = ""
+    user_colorscheme: str = ""
+    generated_colorscheme: str = ""
+    betterdiscord_theme: str = ""
 
-args = parser.parse_args()
-if args.wallpaper.count(".") != 1:
-    parser.error("Can't work with this format, I don't want to deal with .")
+    @staticmethod
+    def setup():
+        cache_dir = getenv("XDG_CACHE_HOME", expanduser("~/.cache"))
+        config_dir = getenv("XDG_CONFIG_HOME", expanduser("~/.config"))
+        state_dir = getenv("XDG_STATE_HOME", expanduser("~/.local/state"))
 
-home_directory = expandvars("${HOME}")
-wallpaper_path = f"{home_directory}/Pictures/Wallpapers/{args.wallpaper}"
+        Dirs.wallpaper = expanduser("~/Pictures/Wallpapers")
+        Dirs.hyprpaper_state = f"{state_dir}/hyprpaper"
+        Dirs.user_colorscheme = f"{config_dir}/colorschemes"
+        Dirs.generated_colorscheme = f"{cache_dir}/wal"
+        Dirs.betterdiscord_theme = f"{config_dir}/BetterDiscord/themes"
+
+    @staticmethod
+    def check():
+        assert Dirs.wallpaper != ""
+        assert Dirs.hyprpaper_state != ""
+        assert Dirs.user_colorscheme != ""
+        assert Dirs.generated_colorscheme != ""
+        assert Dirs.betterdiscord_theme != ""
+
+def parse_arguments() -> argparse.Namespace:
+    # Argument parser setup
+    parser = argparse.ArgumentParser(
+        description="Apply a pywal-compatible theme from a JSON colorscheme."
+    )
+    parser.add_argument(
+        "wallpaper",
+        help="wallpaper filename (with extension) used to identify theme name."
+    )
+    parser.add_argument(
+        "-l", "--light",
+        action="store_true",
+        help="Use light variant of the colorscheme (default: dark variant)"
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Print the resolved path and command without applying theme"
+    )
+
+    args = parser.parse_args()
+
+    if args.wallpaper.count(".") != 1:
+        parser.error("Can't work with this format, I don't want to deal with .")
+
+    return args
+
+def remove_extension(path: str) -> str:
+    return path[:path.find(".")]
+
+def apply_wal(args: argparse.Namespace):
+    if args.light:
+        colorscheme_path = f"{Dirs.user_colorscheme}/{remove_extension(args.wallpaper)}.light.json"
+    else:
+        colorscheme_path = f"{Dirs.user_colorscheme}/{remove_extension(args.wallpaper)}.dark.json"
+
+    if not isfile(colorscheme_path):
+        print(f'User colorscheme at: "{colorscheme_path}" not found', file=stderr)
+        return
+
+    run(["wal", "-f", colorscheme_path], check=True)
 
 
-config_directory = getenv("XDG_CONFIG_HOME")
-if config_directory is None:
-    config_directory = expandvars("${HOME}/.config")
+def apply_hyprpaper(args: argparse.Namespace):
+    makedirs(Dirs.hyprpaper_state, exist_ok=True)
+    state_file = f"{Dirs.hyprpaper_state}/active_wallpaper"
 
-colorscheme_directory = f"{config_directory}/colorschemes"
+    if isfile(state_file):
+        remove(state_file)
 
-wallpaper_name = args.wallpaper[:args.wallpaper.find(".")]
-if args.light:
-    colorscheme_path = f"{colorscheme_directory}/{wallpaper_name}.light.json"
-else:
-    colorscheme_path = f"{colorscheme_directory}/{wallpaper_name}.dark.json"
+    symlink(f"{Dirs.wallpaper}/{args.wallpaper}", state_file)
+    run(["hyprctl", "hyprpaper", "reload", f",{state_file}"], check=True)
 
-print("Wallpaper path:", wallpaper_path)
-print("Colorscheme path:", colorscheme_path)
 
-if not isfile(wallpaper_path):
-    print("Wallpaper file is not present", file=stderr)
-    exit(1)
+def main():
+    args: argparse.Namespace = parse_arguments()
 
-if not isfile(colorscheme_path):
-    print("Colorscheme file is not present", file=stderr)
-    exit(1)
+    Dirs.setup()
+    apply_wal(args)
+    apply_hyprpaper(args)
 
-if args.dry_run:
-    exit(0)
-
-run(["hyprctl", "hyprpaper", "reload", f",{wallpaper_path}"], check=True)
-run(["wal", "-f", colorscheme_path], check=True)
-
-hyprpaper_directory = f"{home_directory}/.local/state/hyprpaper"
-makedirs(hyprpaper_directory, exist_ok=True)
-
-hyprpaper_active_symlink = f"{hyprpaper_directory}/active_wallpaper"
-remove(hyprpaper_active_symlink)
-symlink(wallpaper_path, hyprpaper_active_symlink)
+if __name__ == "__main__":
+    main()
